0fedf58947436752f779106f82c73e87e8183a62
Mariusz <mariusz@berriesinthebasket.com>
2025-05-16 15:26:21 +0200

    Initial Commit

---
+++ WeatherAgent/WeatherAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAgentApp.swift
+//  WeatherAgent
+//
+//  Created by Mariusz Smoliński on 16.05.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
